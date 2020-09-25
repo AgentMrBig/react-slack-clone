8d8170bd0227783a20273f4f44b57360859f9ef1
AgentMrBig <agentmrbig@gmail.com>
2020-09-25 01:03:49 -0400

    Got basic routing setup with react router dom, the tutorial has all routing done in index.js which is blasphemy so I'm doing it like reactrouter suggests, in app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Home from './components/Home';
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
+
+const App = () => {
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    </Router>
+  );
+
 }
 
 export default App;
